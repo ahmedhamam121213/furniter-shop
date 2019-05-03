910b627920eb907f29c842f81ac7b1a6d286c030
ahmed hamam <ahmedhamam@ain-mail.com>
2019-05-03 16:28:15 +0200

    add firebase configs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDLr-lYNgoJErZJKzASHDnnCkV0AUKToes",
+    authDomain: "furnitureshop-883f6.firebaseapp.com",
+    databaseURL: "https://furnitureshop-883f6.firebaseio.com",
+    projectId: "furnitureshop-883f6",
+    storageBucket: "furnitureshop-883f6.appspot.com",
+    messagingSenderId: "1087578784710"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDLr-lYNgoJErZJKzASHDnnCkV0AUKToes",
+    authDomain: "furnitureshop-883f6.firebaseapp.com",
+    databaseURL: "https://furnitureshop-883f6.firebaseio.com",
+    projectId: "furnitureshop-883f6",
+    storageBucket: "furnitureshop-883f6.appspot.com",
+    messagingSenderId: "1087578784710"
+  }
 };
 
 /*
