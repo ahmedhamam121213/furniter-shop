4868414b7b44bca8d4f864298dc649f67a3343a7
ahmed hamam <ahmedhamam@ain-mail.com>
2019-05-04 17:26:40 +0200

    add custom search products

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,19 +1,39 @@
 import { ProductService } from './../../product.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent implements OnInit {
+export class AdminProductsComponent implements OnInit , OnDestroy {
 
-  products$;
+  products:any[];
+  filteredProducts:any[];
+  subscription:Subscription;
   constructor( private productServ : ProductService ) { 
-    this.products$ = productServ.getAll();
+     this.subscription = productServ.getAll().subscribe( products => {
+     this.filteredProducts = this.products = products
+    })
+   }
+   search(query){
+
+    if(query){
+      this.filteredProducts = this.products.
+      filter( p => p.payload.val().title.toLowerCase().includes(query.toLowerCase()) );
+    }else{
+      this.filteredProducts = this.products
+    }
+
    }
 
   ngOnInit() {
+  }
+  ngOnDestroy(){
+
+    this.subscription.unsubscribe();
+    
   }
   
 }
